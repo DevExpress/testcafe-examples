eccdd728955745cff4098a6d58e7d5465af7c4a6
Mikhail Losev <mlosev86@gmail.com>
2020-06-05 18:02:21 +0300

    fix tests (#14)

--- examples/element-properties/check-element-markup.js
+++ examples/element-properties/check-element-markup.js
@@ -3,12 +3,12 @@ import { Selector } from 'testcafe';
 fixture `Element properties`
     .page('https://devexpress.github.io/testcafe/example/');
 
-test('Check an element\'s markup', async t => {
+test("Check an element's markup", async t => {
     const selector = Selector('label[for]').addCustomDOMProperties({
         outerHTML: el => el.outerHTML
     });
 
     const elementOuterHTML = await selector().outerHTML;
 
-    await t.expect(elementOuterHTML).eql('<label for="remote-testing"><input type="checkbox" name="remote" id="remote-testing">Support for testing on remote devices</label>');
+    await t.expect(elementOuterHTML).eql('<label for="remote-testing"><input type="checkbox" name="remote" id="remote-testing" data-testid="remote-testing-checkbox">Support for testing on remote devices</label>');
 });
