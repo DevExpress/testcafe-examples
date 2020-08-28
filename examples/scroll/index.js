3db19f4e990c4e2391d1e4827ff94b93bd07ed2b
Mikhail Losev <mlosev86@gmail.com>
2020-08-28 14:22:24 +0300

    Scroll (#19)
    * scroll example
    
    * renames
    
    Co-authored-by: MargaritaLoseva <margarita.loseva@devexpress.com>

---
+++ examples/scroll/index.js
@@ -0,0 +1,33 @@
+import { Selector, ClientFunction } from 'testcafe';
+
+fixture `Scroll page`
+    .page `./index.html`
+    .beforeEach(async t => {
+        await t.resizeWindow(800, 600);
+    });
+
+const btn1 = Selector('#btn1');
+const btn2 = Selector('#btn2');
+
+const scrollToElement = ClientFunction((selector, offsetX, offsetY) => {
+    const element = selector();
+
+    if (element && element.scrollIntoView)
+        element.scrollIntoView();
+
+    else if (offsetX !== void 0 && offsetY !== void 0)
+        window.scrollBy(offsetX, offsetY);
+});
+
+const scrollBy = ClientFunction((x, y) => {
+    window.scrollBy(x, y);
+});
+
+test('Scroll with an action', async t => {
+    await t.hover(btn1);
+});
+
+test('Scroll with a ClientFunction', async t => {
+    await scrollToElement(btn2, 250, 0);
+    await scrollBy(2000, 0);
+});
