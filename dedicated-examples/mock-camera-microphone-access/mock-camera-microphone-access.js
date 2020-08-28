1743d20df9f5e7e497a70989ba5569ce43c67afd
MargaritaLoseva <margarita.loseva@devexpress.com>
2020-08-28 11:50:20 +0300

    mock camera microphone acess

---
+++ dedicated-examples/mock-camera-microphone-access/mock-camera-microphone-access.js
@@ -0,0 +1,25 @@
+import { Selector } from 'testcafe';
+
+fixture`Mock microphone/camera access`
+    .page('./index.html')
+
+test('take a photo', async t => {
+    const photoButton = Selector('#photoButton')
+    const photoFrame  = Selector('#photo').addCustomDOMProperties({
+        src: el => el.src
+    });
+
+    await t
+        .click(photoButton)
+        .expect(photoFrame.src).match(/data:image\/png;base64,.*/);
+});
+
+test('check an audio element source', async t => {
+    const audioFrame = Selector('audio').addCustomDOMProperties({
+        //this property is true element has source, otherwise false
+        hasSource: el => !!el.srcObject
+    });
+
+    await t
+        .expect(audioFrame.hasSource).ok();
+});
