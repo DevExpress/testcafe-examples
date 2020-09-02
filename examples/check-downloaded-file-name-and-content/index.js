69e6da6c6d4f06fe47d8dfa586dd18a979b17130
Sergey Afonchenko <49588154+DIRECTcut@users.noreply.github.com>
2020-09-02 13:58:07 +0300

    Switched to static file download, added "Check downloaded file content"

--- examples/check-downloaded-file-name-and-content/index.js
+++ examples/check-downloaded-file-name-and-content/index.js
@@ -1,12 +1,14 @@
 import { RequestLogger } from 'testcafe';
 
-const url = 'http://localhost:3000/download-random-file';
+const url = 'http://localhost:3000/download-file';
 
 const logger = RequestLogger({ url, method: 'GET' }, {
     logResponseHeaders: true,
+    logResponseBody: true,
+    stringifyResponseBody: true
 });
 
-fixture `Download file with dynamic name and content`
+fixture `Download file`
     .page('./index.html')
     .requestHooks(logger);
 
@@ -14,7 +16,8 @@ test('Check file name', async t => {
 
     await t
         .click('#generate-btn')
-        .expect(logger.contains(r => r.response.statusCode === 200)).ok();
+        .expect(logger.contains(r.response.statusCode === 200)).ok();
 
     console.log(logger.requests[0].response.headers['content-disposition']);
+    console.log(logger.requests[0].response.body);
 });
