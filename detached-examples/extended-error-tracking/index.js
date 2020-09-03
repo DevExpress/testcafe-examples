4b5a72269efb8093d55f6eee8f3548c87975bdcd
Sergey Afonchenko <49588154+DIRECTcut@users.noreply.github.com>
2020-09-03 14:20:29 +0300

    Add comment

--- detached-examples/extended-error-tracking/index.js
+++ detached-examples/extended-error-tracking/index.js
@@ -9,6 +9,8 @@ test('Extended error tracking', async t => {
     const errors        = await t.eval(() => window.capturedErrors);
     const foundCritical = errors.some(error => error.level === 'critical' && error.url === 'https://my-domain.example.com');
     
+    // Use the `notOk` method to fail the test.
+    // We use the `ok` method to keep all tests green.
     await t.expect(foundCritical).ok("Critical errors occurred during test");         
 })
 .clientScripts('./setup-collecting-errors.js');
