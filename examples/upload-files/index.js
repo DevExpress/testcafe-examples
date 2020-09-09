676e68e2076e7638a3cf25017ad04e2b0820e027
Sergey Afonchenko <49588154+DIRECTcut@users.noreply.github.com>
2020-09-09 13:19:51 +0300

    Update index.js

--- examples/upload-files/index.js
+++ examples/upload-files/index.js
@@ -6,22 +6,20 @@ fixture `My fixture`
     .page `./index.html`
     .requestHooks(logger);
 
-test('Select files to upload', async t => {
+test('Select multiple files to upload', async t => {
     await t
         .setFilesToUpload('#upload-input', [
             './uploads/text-file.txt',
             './uploads/text-file2.txt'
         ])
         .click('#btn');
-
     await t
         .expect(logger.requests[0].response.statusCode).eql(200);
 });
 
-test('Upload nothing', async t => {
+test('Select no files to upload', async t => {
     await t
         .click('#btn');
-        
     await t
         .expect(logger.requests[0].response.statusCode).notEql(200);
 });
