2f7f25c4ab0d3d1b5c8a580ac0346e6b38b0dca8
Sergey Afonchenko <49588154+DIRECTcut@users.noreply.github.com>
2020-09-16 11:49:28 +0300

    Fix tests

--- examples/check-if-opened-with-testcafe/index.js
+++ examples/check-if-opened-with-testcafe/index.js
@@ -4,5 +4,5 @@ fixture `Check to ensure that the page was opened with TestCafe`
     .page('./index.html')
 
 test('Check to ensure that the page was opened with TestCafe', async t => {
-    await t.expect(Selector('#main').innerText).eql('The page is opened with TestCafe: yes');
+    await t.expect(Selector('#main').innerText).eql('The page was opened with TestCafe: yes');
 });
