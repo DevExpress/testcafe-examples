2e81b721dce95993e9027d574d566cc97b0304c4
Sergey Afonchenko <49588154+DIRECTcut@users.noreply.github.com>
2020-09-22 11:00:44 +0300

    Add "How to capture client-side errors on a page" (#24)
    * docs: add "How to capture client-side errors on a page"
    
    Co-authored-by: arminal <arminal@devexpress.com>

---
+++ detached-examples/extended-error-tracking/setup-collecting-errors.js
@@ -0,0 +1,5 @@
+window.capturedErrors = [];
+
+window.onerror = (message, source, lineno, colno, error) => {
+    window.capturedErrors.push({ message, source, lineno, colno, error, level: error.level, url: error.url });
+}
