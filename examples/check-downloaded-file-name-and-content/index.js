ad35da168d9f1a421cc4e532eff22e3728804f39
Sergey Afonchenko <49588154+DIRECTcut@users.noreply.github.com>
2020-09-22 11:01:52 +0300

    Add "How to check downloaded file" (#25)
    * docs: Add "How to check downloaded file name"
    
    Co-authored-by: Albert Totten <49917542+albertov05@users.noreply.github.com>

---
+++ examples/check-downloaded-file-name-and-content/index.js
@@ -0,0 +1,42 @@
+import { RequestLogger } from 'testcafe';
+
+const url = 'http://localhost:3000/download-file';
+
+const logger = RequestLogger({ url, method: 'GET' }, {
+    logResponseHeaders:     true,
+    logResponseBody:        true,
+    stringifyResponseBody:  true
+});
+
+fixture `Download file`
+    .page('./index.html')
+    .requestHooks(logger);
+
+test('Check file name and content', async t => {
+    const fileNameRegEx = /attachment; filename=.*.txt/;
+    
+    await t
+        .click('#download-btn')
+        .expect(logger.contains(r => {
+            if (r.response.statusCode !== 200)
+                return false;
+           
+            const requestInfo = logger.requests[0];
+           
+            if (!requestInfo)
+                return false;
+      
+            const downloadedFileName = requestInfo.response.headers['content-disposition'];
+            
+            if (!downloadedFileName)
+                false;
+      
+             if (!fileNameRegEx.test(downloadedFileName))
+                  return false;
+            
+             const downloadedFileContent = logger.requests[0].response.body;
+      
+             return downloadedFileContent  === 'Test content';
+      })).ok();
+});
+ 
