b7f5921eabf04c1df72272258aa0d91b59b7f9d3
Dmitrii <ddm157@yandex.ru>
2020-11-02 14:58:55 +0300

    Changed the example to use the smart query assertion mechanism

--- examples/get-content-from-multiple-elements/index.js
+++ examples/get-content-from-multiple-elements/index.js
@@ -3,19 +3,15 @@ import { Selector } from 'testcafe';
 fixture `Get content from multiple elements`
     .page `./index.html`;
 
-async function getTextFromElements (selector) {
-    const result = [];
-
-    for (let i = 0; i < await selector.count; i++) {
-        result.push(await selector.nth(i).textContent);
-    }
-
-    return result;
-}
-
 test('Get text from all items in the <ul> list', async t => {
+    const expectedContent    = ['hello', 'word', '!'];
     const liElementsSelector = Selector('#ul').find('li');
 
-    await t
-        .expect(await getTextFromElements(liElementsSelector)).eql(['hello', 'world', '!']);
+    await t.expect(liElementsSelector.count).eql(expectedContent.length);
+
+    const nodeCount = await liElementsSelector.count;
+
+    for (let i = 0; i < nodeCount; i++) {
+        await t.expect(liElementsSelector.nth(i).textContent).eql(expectedContent[i]);
+    }
 });
