18707accd55b84c213b58e5ee2ab1907dbc67cd0
Dmitrii Peredreev <ddm157@yandex.ru>
2020-11-03 17:34:34 +0300

    fixed a typo

--- examples/get-content-from-multiple-elements/index.js
+++ examples/get-content-from-multiple-elements/index.js
@@ -4,7 +4,7 @@ fixture `Get content from multiple elements`
     .page `./index.html`;
 
 test('Get text from all items in the <ul> list', async t => {
-    const expectedContent    = ['hello', 'word', '!'];
+    const expectedContent    = ['hello', 'world', '!'];
     const liElementsSelector = Selector('#ul').find('li');
 
     await t.expect(liElementsSelector.count).eql(expectedContent.length);
