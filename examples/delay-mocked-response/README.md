0d84d4aa044f4014e7bd40c2633f2e78ea354eed
Pavel <wabloxa@yandex.ru>
2021-04-16 11:49:51 +0300

    add: an example on how to delay a mocked response (#54)
    * add: an example on how to delay mocked response
    
    * Update examples/delay-mocked-response/README.md
    
    Co-authored-by: Sergey Afonchenko <49588154+DIRECTcut@users.noreply.github.com>
    
    Co-authored-by: Sergey Afonchenko <49588154+DIRECTcut@users.noreply.github.com>

---
+++ examples/delay-mocked-response/README.md
@@ -0,0 +1,5 @@
+# Delay Mocked Response
+
+**Test Code**: [delay-mock-response.js](delay-mocked-response.js)
+
+This example shows how to delay a mocked response for particular requests by the given duration.
---
+++ examples/delay-mocked-response/delay-mocked-response.js
@@ -0,0 +1,27 @@
+import { Selector, RequestMock } from 'testcafe';
+import delay from 'delay';
+
+const MOCK_RESPONSE_DELAY = 5000;
+
+const mock = RequestMock()
+    .onRequestTo(/request/)
+    .respond(async (req, res) => {
+        await delay(MOCK_RESPONSE_DELAY);
+
+        res.setBody('mocked content');
+    });
+
+fixture `Delay mocked response`
+    .page `http://localhost:3000/examples/delay-mocked-response/index.html`;
+
+test.requestHooks(mock)
+('Check that the mocked response was delayed', async (t) => {
+    await t
+        .click('#send-request')
+        .expect(Selector('#response').textContent)
+        .eql('mocked content', { timeout: MOCK_RESPONSE_DELAY + 3000 });
+
+    const elapsedTime = await Selector('#elapsed-time').textContent;
+
+    await t.expect(parseInt(elapsedTime, 10)).gte(MOCK_RESPONSE_DELAY);
+});
