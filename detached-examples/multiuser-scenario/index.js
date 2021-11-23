850e6774c229aab914d1938891f3b45cb46a5e43
Ilya Afanasenko <fg1@mail.ru>
2021-11-23 16:49:37 +0300

    fix: clearing npm cache no longer interferes with loading scenarios

--- detached-examples/multiuser-scenario/index.js
+++ detached-examples/multiuser-scenario/index.js
@@ -1,6 +1,5 @@
 const createTestCafe = require('testcafe');
 
-
 class User {
     constructor (name, fileName, browser) {
         this.name     = name;
@@ -44,7 +43,9 @@ class User {
     }
 }
 
-const scenarios = new Map();
+const scenarios = global.scenarios || new Map();
+
+global.scenarios = scenarios;
 
 class Scenario {
     constructor (description) {
