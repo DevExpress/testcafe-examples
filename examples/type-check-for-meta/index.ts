22cbbfd3b870c9de17555737e51c71ef238320c3
Adil Rakhaliyev <67043367+Bayheck@users.noreply.github.com>
2023-10-04 20:05:00 +0600

    Update examples/type-check-for-meta/index.ts
    Co-authored-by: aleks-pro <alexander.prokhorov@hotmail.com>

--- examples/type-check-for-meta/index.ts
+++ examples/type-check-for-meta/index.ts
@@ -19,8 +19,8 @@ fixture `Fixture with meta device: desktop`
 test
     .meta({ device: 'mobile', skip: false })
     ('test with meta device: mobile and skip: false', async t => {
-    await t.wait(1000);
-});
+        await t.wait(1000);
+    });
 
 test
     .meta<MetaOptions>({ device: 'desktop', skip: false })
