8aaec0ef9709efc3633cce4db15556f446d142e7
Bayheck <adil16052000@gmail.com>
2023-10-05 11:04:34 +0600

    fix: pr comments fixed

--- examples/type-check-for-meta/custom-typing.ts
+++ examples/type-check-for-meta/custom-typing.ts
@@ -2,14 +2,11 @@ type MetaOptions = {
     device: 'mobile' | 'desktop' | 'tablet';
     skip: boolean;
 };
-  
+
 declare module 'testcafe' {
     global {
         interface TestFn {
             meta(options: MetaOptions): this;
-        }
-
-        interface TestFn {
             meta<T>(options: T): this;
         }
     }
