700f23d817e1fcacae7d64dcc7d13bc376ee6d34
Bayheck <adil.rakhaliyev@devexpress.com>
2024-11-07 16:20:04 +0500

    fix: tests fixed

--- examples/filter-tests-by-metadata/index.js
+++ examples/filter-tests-by-metadata/index.js
@@ -3,8 +3,7 @@ fixture`The metadata of this fixture satisfies both exclusion requirements. Test
     .page`https://devexpress.github.io/testcafe/example`;
 
 test
-    .meta({ device: 'desktop', skip: false })
-    ('The metadata of this test does not satisfy either exclusion requirement. The test does not run because TestCafe skips the fixture', async t => {
+    .meta({ device: 'desktop', skip: false })('The metadata of this test does not satisfy either exclusion requirement. The test does not run because TestCafe skips the fixture', async t => {
         await t.wait(1000);
     });
 
@@ -13,8 +12,7 @@ fixture`The metadata of this fixture satisfies one exclusion requirement. TestCa
     .page`https://devexpress.github.io/testcafe/example`;
 
 test
-    .meta({ device: 'desktop', skip: false })
-    ('The metadata of this test does not satisfy either exclusion requirement. The test does not run because TestCafe skips the fixture', async t => {
+    .meta({ device: 'desktop', skip: false })('The metadata of this test does not satisfy either exclusion requirement. The test does not run because TestCafe skips the fixture', async t => {
         await t.wait(1000);
     });
 
@@ -23,18 +21,15 @@ fixture`The metadata of this fixture doesn't satisfy either exclusion requiremen
     .page`https://devexpress.github.io/testcafe/example`;
 
 test
-    .meta({ device: 'mobile', skip: true })
-    ("The metadata of this test satisfies both exclusion requirements. TestCafe skips this test.", async t => {
+    .meta({ device: 'mobile', skip: true })('The metadata of this test satisfies both exclusion requirements. TestCafe skips this test.', async t => {
         await t.wait(1000);
     });
 
 test
-    .meta({ device: 'mobile', skip: false })
-    ("The metadata of this test satisfies one exclusion requirement. TestCafe skips this test.", async t => {
+    .meta({ device: 'mobile', skip: false })('The metadata of this test satisfies one exclusion requirement. TestCafe skips this test.', async () => {
     });
 
 test
-    .meta({ device: 'desktop', skip: false })
-    ("The metadata of this test doesn't satisfy either exclusion requirement. TestCafe runs this test.", async t => {
+    .meta({ device: 'desktop', skip: false })("The metadata of this test doesn't satisfy either exclusion requirement. TestCafe runs this test.", async t => {
         await t.wait(1000);
     });
